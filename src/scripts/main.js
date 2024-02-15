8079822dd612037ccf122c8c6d15852b2a438b4e
Diogo Peres <plaer654@gmail.com>
2024-02-15 20:55:06 -0300

    duvida sobre responsividade dentro do slick

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,5 +6,20 @@ $(document).ready(function () {
         autoplaySpeed: 2000,
         slidesToShow: 4,
         slidesToScroll: 4,
+        resposive: [
+            {
+                breakpoint: 430,
+                settings: {
+                    slidesToShow:2,
+                    slidesToScroll:2,
+                }
+            }
+        ]
     });
+
+    window.addEventListener('scroll', function () {
+        let header = document.querySelector('#header')
+
+        header.classList.toggle('roll', window.scrollY > 850)
+    })
 });
