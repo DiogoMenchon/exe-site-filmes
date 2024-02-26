4f72a06d32349b500b4fcba7eddc7a3c4e6637ef
Diogo Peres <plaer654@gmail.com>
2024-02-26 10:34:50 -0300

    troca para boostrap

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,6 @@
-$(document).ready(function () {
-    $('.corousel').slick({
-        dots: true,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        resposive: [
-            {
-                breakpoint: 430,
-                settings: {
-                    slidesToShow:2,
-                    slidesToScroll:2,
-                }
-            }
-        ]
-    });
 
     window.addEventListener('scroll', function () {
         let header = document.querySelector('#header')
 
         header.classList.toggle('roll', window.scrollY > 850)
     });
-});
